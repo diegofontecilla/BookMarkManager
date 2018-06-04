2557115621f8fc1cac5ecaa7a4edc7a64a441cde
arsalanabc <arsalanabc@gmail.com>
2018-06-04 15:48:04 +0100

    showing links

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,7 +1,13 @@
 feature 'viewing links' do
+	
 
 	scenario 'view links on the page' do
-		visit('/')
 		
+		
+		visit('/')
+
+		expect(page).to have_content('www.google.com')
+		expect(page).to have_content('www.reddit.com')
+		expect(page).to have_content('www.this.com')
 	end
 end
